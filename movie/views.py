40dcf2c5c7f0f8f79fc2bc7956ed6c9156379ed4
Valborg Johannesen <2328244j@student.gla.ac.uk>
2022-03-11 12:46:01 +0000

    Worked on movie view

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from movie.models import Movie
+
 
 
 
@@ -9,5 +11,18 @@ from django.shortcuts import render
 
 
 def movie(request):
+
+    movie_list = Movie.objects.order_by('-title')
+
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'movie/allmovies.html', context=context_dict)
+    context_dict['movies'] = movie_list
+
+    response = render(request, 'movie/allmovies.html', context=context_dict)
+
+    return response
+
+
+# def show_movies(request, movie_name_slug):
+
+# @login_required
+# def add_movie()
